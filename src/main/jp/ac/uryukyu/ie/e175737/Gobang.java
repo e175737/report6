8d05a4c3eb317971b13567c720b20bc5848c96fa
Ayaka Toma <e175737@ie.u-ryukyu.ac.jp>
2020-02-24 15:56:54 +0900

    入力を受け取る処理を追加

--- src/main/jp/ac/uryukyu/ie/e175737/Gobang.java
+++ src/main/jp/ac/uryukyu/ie/e175737/Gobang.java
@@ -1,23 +1,17 @@
 package jp.ac.uryukyu.ie.e175737;
 
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+import java.util.Scanner;
+
 public class Gobang {
     public int[][] board;
+    public String[] yAxis = {"A", "B", "C", "D", "E", "F", "G", "H"};//ｙ軸用のラベル
+
 
     /*コンストラクタ*/
     public Gobang() {
         // 盤面状態
-        /*int[][] board = {
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-        };*/
-
-
         int size = 8;
         board = new int[size][size];
         for (int i = 0; i < size; i++) {
@@ -27,11 +21,9 @@ public class Gobang {
         }
     }
 
+    // 盤面表示
     public void display(Gobang gobang){
-        String[] yAxis = {"A", "B", "C", "D", "E", "F", "G",
-                "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"};//ｙ軸用のラベル
 
-        // 盤面表示
         System.out.println();
         System.out.println("     1   2   3   4   5   6   7   8");
         System.out.println("   +---+---+---+---+---+---+---+---+");
@@ -53,5 +45,43 @@ public class Gobang {
         System.out.println("\n   black：player1   white：player2");
     }
 
+
+    public void play(Gobang gobang, int player) {
+
+        System.out.println("\n ###################################\n");
+        System.out.println("player" + player + "の番です。置く場所を入力してください。");
+        System.out.println("入力例：A1");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        String[] ar = input.split("");
+
+        /*指定した入力か確認*/
+        if(input.matches("^[A-H][1-8]$")) {
+            int x = Integer.parseInt(ar[1]) - 1;
+
+            for (int i = 0; i < 8; i++) {
+                if (yAxis[i].equals(ar[0])) {
+
+                    /*すでに置かれている場所に置けないようにする*/
+                    if (gobang.board[i][x] == 0){
+                        gobang.board[i][x] = player;
+                        gobang.display(gobang);
+                    } else{
+                        System.out.println("すでに置かれている場所です。");
+                    }
+                    break;
+                }
+            }
+
+        } else {
+            System.out.println("例のように入力してください。");
+        }
+    }
+
+
+
 }
+
+
+
 
--- src/main/jp/ac/uryukyu/ie/e175737/Main.java
+++ src/main/jp/ac/uryukyu/ie/e175737/Main.java
@@ -1,13 +1,16 @@
 package jp.ac.uryukyu.ie.e175737;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-    Gobang gobang = new Gobang();
+        Gobang gobang = new Gobang();
 
-    gobang.board[0][0]=2;
-    gobang.board[7][7]= 1;
-    gobang.display(gobang);
+        gobang.display(gobang);
 
+        for (int i = 0; i < 3; i++) {
+            gobang.play(gobang, 1);
+            gobang.play(gobang, 2);
+        }
     }
 }
